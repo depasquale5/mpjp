8ee2185bee47e6cd9a91b0c5fa8a41dfd50fb458
Davide <d.depasquale94@gmail.com>
2020-06-07 19:06:26 +0200

    Added template for S07.java and S07Test.java

---
+++ src/main/java/m2ex/S07.java
@@ -0,0 +1,36 @@
+package m2ex;
+
+public class S07 {
+	/**
+	 * 
+	 * @param value
+	 * @return true if value is a perfect number. A perfect number is a positive
+	 *         integer that is equal to the sum of its positive divisors, excluding
+	 *         the number itself. Example: 6 = 1 + 2 + 3 28 = 1 + 2 + 4 + 7 + 14
+	 */
+	public static boolean isPerfect(int value) {
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param value
+	 * @return true if value is an abundant number. An abundant number or excessive
+	 *         number is a number for which the sum of its proper divisors is
+	 *         greater than the number itself.
+	 */
+	public static boolean isAbundant(int value) {
+		return false;
+	}
+
+	/**
+	 * 
+	 * @param value
+	 * @return true if value is a deficient number. A deficient number or defective
+	 *         number is a number for which the sum of its proper divisors is
+	 *         less than the number itself.
+	 */
+	public static boolean isDeficient(int value) {
+		return false;
+	}
+}
---
+++ src/test/java/m2ex/S07Test.java
@@ -0,0 +1,23 @@
+package m2ex;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class S07Test {
+
+	@Test
+	void testIsPerfect() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsAbundant() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testIsDeficient() {
+		fail("Not yet implemented");
+	}
+}
